24cc82113a307209da670f5b8c7fd8d1caf52490
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 11:46:05 -0700

    created navbar from materialize template

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="brand-logo">
+            Emaily
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
