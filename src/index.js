e24432a248fee7da8883140dc167d186043f69d1
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 11:09:51 -0700

    created template dummy reducer as a placeholder (to be replaced later)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+// react
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
+// redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 
+const store = createStore(() => [], {}, applyMiddleware()); // dummy store for the time being
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
