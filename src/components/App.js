ebed7d70f91dc1533cd7cae3e8323081f5802deb
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 11:30:41 -0700

    stubbed out basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-function App() {
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h1>Header</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+const SurveyNew = () => <h1>Surveys New</h1>;
+//const Surveys = () => <h1>Surveys</h1>;
+const Landing = () => <h1>Landing</h1>;
+
+const App = () => {
   return (
-    <div className="App">
-      Hi There
-      <a href="http://localhost:5000/auth/google">Sign in with Google</a>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
