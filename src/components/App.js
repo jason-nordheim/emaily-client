18ee5cf153430f00eee4274ff2cb68c0a9665560
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 11:06:21 -0700

    stripped out template files

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      Hi There
+      <a href="http://localhost:5000/auth/google">Sign in with Google</a>
+    </div>
+  );
+}
+
+export default App;
