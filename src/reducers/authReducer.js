bc9ff920b0a33fad6502a18353aa12f6b1d2335a
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 14:23:13 -0700

    added fetch user action to authReducer.js

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,13 @@
-export default function authReducer(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+/** state initialized to null, indicating that no user
+ * is currently authenticated
+ */
+export default function authReducer(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      /** if empty string, return a false value  */
+      return action.payload || false;
     default:
       return state;
   }
