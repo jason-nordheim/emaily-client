f8d59625c6a7e50fa2804c3d9d96c303c9c9a256
jason-nordheim <jason.nordheim@gmail.com>
2021-01-28 12:01:15 -0700

    converted App component to a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { Component } from "materialize-css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 
@@ -6,19 +7,21 @@ const SurveyNew = () => <h1>Surveys New</h1>;
 //const Surveys = () => <h1>Surveys</h1>;
 const Landing = () => <h1>Landing</h1>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
